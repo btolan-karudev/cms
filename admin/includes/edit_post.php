9d20e414d69cdca4bf4db4e50beaf6c2a0bf8212
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-11-03 20:15:47 +0000

    image a resoudre

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -15,10 +15,11 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_comment_count = $row['post_comment_count'];
     $post_date = $row['post_date'];
     $post_image = $row['post_image'];
+    $post_content = $row['post_content'];
     $post_tags = $row['post_tags'];
 }
 
-
+// WHERE post_id = {$post_id}
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
@@ -26,22 +27,22 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title">
+        <input type="text" value="<?php echo $post_title; ?>" class=" form-control" name="title">
     </div>
 
     <div class="form-group">
-        <label for="category">Post Category</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <label for="category">Post Category ID</label>
+        <input type="text" value="<?php echo $post_category_id; ?>" class="form-control" name="post_category_id">
     </div>
 
     <div class="form-group">
         <label for="author">Post Author</label>
-        <input type="text" value="<?php echo $post_author; ?>" class="form-control" name="author">
+        <input type="text" value="<?php echo $post_author; ?>" class="form-control" name="post_author">
     </div>
 
     <div class="form-group">
         <label for="status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+        <input type="text" value="<?php echo $post_status; ?>" class="form-control" name="post_status">
     </div>
 
     <!--    <div class="form-group">-->
@@ -96,17 +97,18 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="post_image">Post Image</label>
-        <input type="file" name="image">
+        <img width="100px" src="../images/<?php echo $post_image; ?>" alt="">
     </div>
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <input type="text" value="<?php echo $post_tags; ?>" class="form-control" name="post_tags">
     </div>
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control " name="post_content" id="" cols="30" rows="10">
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10">
+            <?php echo $post_content; ?>
          </textarea>
     </div>
 
